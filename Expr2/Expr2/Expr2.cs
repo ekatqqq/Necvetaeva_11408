ec27b9a272108bb1cc8963f97d8e2e8608936728
ekatqqq <wuueqqqwerrrt@gmail.com>
2024-11-11 19:51:47 +0300

    2024\Fall\Inf\HW\24.11.04\Expr2

---
+++ Expr2/Expr2/Expr2.cs
@@ -0,0 +1,21 @@
+﻿namespace Expr2
+{
+    class TaskExecutor
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter a three digit number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetResult(number));
+        }
+
+        static string GetResult(int number)
+        {
+            int num1 = number / 100 % 1000;
+            int num2 = number / 10 % 10;
+            int num3 = number % 10;
+
+            return ($"{num3}{num2}{num1}").ToString();
+        }
+    }
+}
