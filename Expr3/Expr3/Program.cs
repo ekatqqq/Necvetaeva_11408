98ce029ccb50c2ba608dee7615a3052ab224555b
ekatqqq <wuueqqqwerrrt@gmail.com>
2024-11-12 20:31:50 +0300

    fixed task Expr3. Now without branches.

--- Expr3/Expr3/Program.cs
+++ Expr3/Expr3/Program.cs
@@ -14,22 +14,8 @@ namespace Expr3
 
         static int GetResult(int time)
         {
-            if ((time > 0 && time <= 6) || (time > 12 && time <= 18))
-            {
-                int angle = time * 30;
-                return angle;
-            }
-            else if (time >= 7 && time < 12)
-            {
-                int angle = 360 - (time * 30);
-                return angle;
-            }
-            else if (time >= 19 && time <= 23)
-            {
-                int angle = 720 - (time * 30);
-                return angle;
-            }
-            return 0;
+            int angle = 180 - Math.Abs(180 - ((time % 12) * 30));
+            return angle;
         }
     }
 }
