7c1ba550beca2b75974fee69899830f96b569f76
ekatqqq <wuueqqqwerrrt@gmail.com>
2024-11-12 22:50:24 +0300

    2024\Fall\Inf\HW\24.11.04\Expr5

---
+++ Expr5/Expr5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Classwork
+{
+    class Expr5
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int a);
+            int.TryParse(Console.ReadLine(), out int b);
+
+            Console.WriteLine(CheckLeapYear(a, b));
+        }
+
+        static int CheckLeapYear(int a, int b)
+        {
+            int result = (b / 4 - (a - 1) / 4) - (b / 100 - (a - 1) / 100) + (b / 400 - (a - 1) / 400);
+            return result;
+        }
+    }
+}
