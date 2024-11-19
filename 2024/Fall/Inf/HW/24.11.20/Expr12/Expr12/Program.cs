49704f2fbf537491960b532623114cd67314268e
ekatqqq <wuueqqqwerrrt@gmail.com>
2024-11-20 02:25:04 +0300

    Expr12

---
+++ 2024/Fall/Inf/HW/24.11.20/Expr12/Expr12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Homework
+{
+    class Expr12
+    {
+        static void Main()
+        {
+            double h = Convert.ToDouble(Console.ReadLine());
+            double t = Convert.ToDouble(Console.ReadLine());
+            double v = Convert.ToDouble(Console.ReadLine());
+            double x = Convert.ToDouble(Console.ReadLine());
+            double minT = (h - x * t) / (v - x);
+            double maxT = t;
+            Console.WriteLine($"{minT} {maxT}");
+        }
+    }
+}
