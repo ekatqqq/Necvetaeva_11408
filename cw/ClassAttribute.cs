fe1f4669ac2f38e6a08fba7fb7a44c8dc909ff43
ekatqqq <wuueqqqwerrrt@gmail.com>
2025-06-08 22:48:02 +0300

    .

---
+++ cw/ClassAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cw2;
+
+public class ClassAttribute<T> : Attribute
+{
+    private T[] plugs { get; set; }
+    public ClassAttribute(params T[] plugins)
+    {
+        plugs = plugins;
+    }
+}
+
+public class Dependencies<T> where T : class
+{
+    public bool CheckDependencies(T[] values)
+    {
+        if (values == null) return false;
+
+        foreach (var plugin in values)
+        {
+            var foundAttributes = plugin.GetType().GetCustomAttributes(typeof(ClassAttribute<T>), false);
+
+            if (foundAttributes != null && foundAttributes.Length > 0)
+            {
+                var dependencyInfo = foundAttributes[0] as ClassAttribute<T>;
+
+                foreach (var e in dependencyInfo) //извините, я не знаю, как тут сделать, мне надо перебрать из dependencyInfo
+                {
+                    bool exists = false;
+
+                    foreach (var t in values)
+                    {
+                        if (t.GetType() == e as Type)
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+
+                    if (!exists) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ cw/ClassGraph.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+namespace cw2;
+
+class Graph<T>
+{
+    private Dictionary<Type, List<Type>> graph;
+
+    public void BuildGraph(Assembly assembly)
+    {
+        foreach (var type in assembly.GetTypes())
+        {
+            var plugAttr = type.GetCustomAttributes(typeof(ClassAttribute<T>), false);
+
+            if (plugAttr.Length > 0)
+            {
+                graph[type] = new List<Type>();
+
+                var attr = (ClassAttribute<Type>)plugAttr[0];
+                FieldInfo plugsField = attr.GetType().GetField("plugs");
+                var requiredPlugins = (Type[])plugsField.GetValue(attr);
+
+                foreach (var pluginType in requiredPlugins)
+                {
+                    graph[type].Add(pluginType);
+                }
+            }
+        }
+    }
+    public bool CheckCycles()
+    {
+        foreach (var pluginType in graph.Keys)
+        {
+            var visited = new HashSet<Type>();
+            var stack = new Stack<Type>();
+
+            stack.Push(pluginType);
+
+            while (stack.Count > 0)
+            {
+                var current = stack.Pop();
+                if (visited.Contains(current)) continue;
+                visited.Add(current);
+
+                if (graph.ContainsKey(current))
+                {
+                    IEnumerable<Type> dependencies = graph[current];
+                    foreach (var e in dependencies)
+                    {
+                        if (e == pluginType) return true;
+                        if (!visited.Contains(e)) stack.Push(e);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
